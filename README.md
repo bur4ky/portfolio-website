99248d0df8a473f6fd7ba7fbc455ff4d3643d437
BurakYs <brkyhs0@gmail.com>
2026-03-01 19:43:16 +0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Personal Portfolio
+
+A portfolio website built with Astro and Tailwind CSS
+
+## 📺 Demo
+
+[Live Demo](https://buraky.dev)
+
+![GIF Preview](https://cdn.buraky.dev/previews/websites/portfolio.gif)
+
+## 🚀 Getting Started
+
+1. Run the following commands to get started:
+
+   ```bash
+   git clone https://github.com/bur4ky/portfolio-website.git
+   cd portfolio-website
+   bun install
+   ```
+
+2. Change the `src/config.ts` file to your information
+
+3. Run `bun dev` to start the server
---
+++ src/config.ts
@@ -0,0 +1,50 @@
+import go from './assets/go.svg';
+import typescript from './assets/typescript.svg';
+import nodejs from './assets/nodejs.svg';
+import mongodb from './assets/mongodb.svg';
+import svelte from './assets/svelte.svg';
+import react from './assets/react.svg';
+import nextjs from './assets/nextjs.svg';
+import sass from './assets/sass.svg';
+import tailwindcss from './assets/tailwindcss.svg';
+
+export default {
+  name: 'Burak',
+  about: 'A student and a developer from Turkey specialized in web development',
+  profession: 'Full Stack Developer',
+  contactEmail: 'contact@buraky.dev',
+  github: 'bur4ky',
+  meta: {
+    title: 'Burak',
+    description: 'My portfolio website showcasing my projects and skills',
+    favicon: 'https://avatars.githubusercontent.com/u/59135244?size=64'
+  },
+  projects: [
+    {
+      title: 'Spitfire Launcher',
+      description: 'A launcher app for Fortnite and Epic Games',
+      thumbnail: 'https://cdn.buraky.dev/banners/spitfire-launcher-min.png',
+      repository: 'https://github.com/bur4ky/spitfire-launcher',
+      url: 'https://rookie-spitfire.xyz/launcher',
+      tags: ['Svelte', 'Tauri', 'Rust']
+    },
+    {
+      title: 'VidGet',
+      description: 'Multi-platform video and audio downloader',
+      thumbnail: 'https://cdn.buraky.dev/banners/vidget.png',
+      repository: 'https://github.com/bur4ky/VidGet',
+      tags: ['Svelte', 'Tailwind CSS']
+    }
+  ],
+  technologies: [
+    { name: 'Go', icon: go },
+    { name: 'TypeScript', icon: typescript },
+    { name: 'Node.js', icon: nodejs },
+    { name: 'MongoDB', icon: mongodb },
+    { name: 'Svelte', icon: svelte },
+    { name: 'React', icon: react },
+    { name: 'Next.js', icon: nextjs },
+    { name: 'Sass', icon: sass },
+    { name: 'Tailwind', icon: tailwindcss }
+  ]
+};
